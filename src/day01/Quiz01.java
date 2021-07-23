89bde4c8bc24901b768fb90eb423e554a24dea6d
matia005 <matia0529@gmail.com>
2021-07-23 15:07:36 +0900

    인적사항 출력

---
+++ src/day01/Quiz01.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class Quiz01 {
+
+	public static void main(String[] args) {
+		System.out.println("==============================");
+		System.out.println("이름\t나이\t전화번호\t\t회비");
+		System.out.println("==============================");
+		System.out.println("홍길동\t"+15+"\t"+3672+"-"+1234+"\t\\"+2000);
+		System.out.println("------------------------------");
+		System.out.println("총 합계 \t\t\t\t\\1000000");
+		System.out.println("------------------------------");
+		
+	}
+
+}
