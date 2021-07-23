ead181f7bfe8b2e5981f97d9c22122d2349189eb
matia005 <matia0529@gmail.com>
2021-07-23 11:19:09 +0900

    주석 방법

--- src/day01/Ex01.java
+++ src/day01/Ex01.java
@@ -3,8 +3,12 @@ package day01;
 public class Ex01 {
 
 	public static void main(String[] args) {
+		
+		//......
 		System.out.println("안녕하세요");
-		System.out.println("........");
+		System.out.println("자바 시간입니다.");
 
+		System.out.print("안녕하세요");
+		System.out.print("자바 시간입니다.");
 	}
 }
