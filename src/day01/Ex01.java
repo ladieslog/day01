11fa649bbf6f8ef74c526e29206e3039e3863544
matia005 <matia0529@gmail.com>
2021-07-23 10:56:29 +0900

    print 기본 실행

--- src/day01/Ex01.java
+++ src/day01/Ex01.java
@@ -3,8 +3,8 @@ package day01;
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("æ»≥Á«œººø‰");
+		System.out.println("........");
 
 	}
-
 }
